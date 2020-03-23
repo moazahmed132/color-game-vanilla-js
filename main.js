2d1b9e421ca90deab1cf167412ce8501c9d5c485
Moaz Ahmed <moaz.ahmed132@gmail.com>
2020-03-23 22:09:04 +0200

    second commit

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-let numberOfBoxes = 6; //default number
-console.log("numberOfBoxes", numberOfBoxes)
+let numberOfBoxes = 3; //default number
 let title = document.getElementById("rgb");
 let boxContainer = document.querySelector(".center");
 let boxContainerArray = Array.from(boxContainer);
@@ -8,15 +7,6 @@ let hardBtn = document.getElementById("hardBtn");
 let hardBoxes = document.querySelectorAll(".hard");
 let newGame = document.getElementById("new");
 let msg = document.getElementById("msg");
-
-hardButtonMode = () => {
-  easyBtn.style.backgroundColor = '#408E61';
-  hardBtn.style.backgroundColor = 'gray';
-}
-easyButtonMode = () => {
-  easyBtn.style.backgroundColor = 'gray';
-  hardBtn.style.backgroundColor = '#408E61';
-}
 singleCellGenerator = () => {
   return Math.floor(Math.random() * (256 - 0)) + 0;
 }
@@ -25,12 +15,8 @@ generatRandomBox = () => {
 }
 // function to generate rgb color 
 rgbColorGenerator = () => {
-  let firstCell = singleCellGenerator();
-  let secondCell = singleCellGenerator();
-  let thirdCell = singleCellGenerator();
-  return "rgb(" + firstCell + ", " + secondCell + ", " + thirdCell + ")";
+  return `rgb(${singleCellGenerator()}, ${singleCellGenerator()}, ${singleCellGenerator()})`;
 }
-
 // function to generate X numers of rgb colors 
 generateMulitColors = () => {
   let arr = [];
@@ -39,39 +25,26 @@ generateMulitColors = () => {
   }
   return arr;
 }
-
 // function to choose a color
 pickedColor = () => {
-  let random = generatRandomBox();
-  let colors = generateMulitColors();
-  return colors[random];
+  return generateMulitColors()[generatRandomBox()];
 }
-
-
 // function to generate 
 generateBoxes = (numberOfBoxes) => {
-
+  boxContainerArray = [];
+  boxContainer.innerHTML = '';
   let moazColor = pickedColor();
   title.innerHTML = moazColor;
-
-  // color the boxes at first
-
   let pickedBox = generatRandomBox();
-  console.log("generateBoxes -> pickedBox", pickedBox)
-
   for (i = 0; i < numberOfBoxes; i++) {
     let box = document.createElement("div");
     box.classList.add("box");
     box.style.backgroundColor = pickedColor();
     boxContainer.appendChild(box);
     boxContainerArray.push(box);
-    console.log("generateBoxes -> boxContainerArray", boxContainerArray[i]);
   }
-
   boxContainerArray[pickedBox].style.backgroundColor = moazColor;
-
   // matching function
-
   boxContainerArray.forEach((box) => {
     box.addEventListener('click', () => {
       if (box.style.backgroundColor == moazColor) {
@@ -88,32 +61,18 @@ generateBoxes = (numberOfBoxes) => {
   });
 }
 generateBoxes(numberOfBoxes);
-
-// removing boxes function
-removeBoxes = (numberOfBoxes) => {
-  boxContainerArray.forEach((r) => {
-    r.remove();
-
-  });
-}
-//console.log("removeBoxes -> boxContainerArray", boxContainerArray)
-
 hardBtn.addEventListener("click", function () {
-  numberOfBoxes = 6;
-  removeBoxes(numberOfBoxes);
-  generateBoxes(numberOfBoxes);
-  hardButtonMode();
+  hardBtn.classList.add('selected');
+  easyBtn.classList.remove('selected');
+  numberOfBoxes = hardBtn.dataset.type;
+  generateBoxes(hardBtn.dataset.type);
 });
 easyBtn.addEventListener("click", function () {
-  numberOfBoxes = 3;
-  removeBoxes(numberOfBoxes);
-  easyButtonMode();
-  generateBoxes(numberOfBoxes);
-
+  easyBtn.classList.add('selected');
+  hardBtn.classList.remove('selected');
+  generateBoxes(easyBtn.dataset.type);
 });
 newGame.addEventListener("click", function () {
-  numberOfBoxes = 6;
-  removeBoxes(numberOfBoxes);
-  generateBoxes(numberOfBoxes);
-  hardButtonMode();
+  //numberOfBoxes = newGame.dataset.type;
+  generateBoxes(newGame.dataset.type);
 });
